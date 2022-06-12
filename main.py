0b5ecca5177e8260f9f22b19554e6b56b54868bf
Jonathon Meney <79815359+OsmiumCoder@users.noreply.github.com>
2022-06-12 18:15:29 -0300

    added imports for all used functions in main file
    may do in classes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from generate_name import generate_name
-import pygame
+from pygame import quit, Rect, Color, draw, mouse
+from pygame import event as ev
 from pygame.locals import *
+from pygame.display import set_mode, set_caption, update
+from pygame.font import init, Font
+from pygame.image import load
+
+from generate_name import generate_name
 from button import Button
 from name import Name
 
@@ -9,24 +14,24 @@ def main():
     # SCREEN
     screen_height = 600
     screen_width = 1000
-    screen = pygame.display.set_mode((screen_width, screen_height), RESIZABLE)
-    pygame.display.set_caption("Capital and City Name Generator")
+    screen = set_mode((screen_width, screen_height), RESIZABLE)
+    set_caption("Capital and City Name Generator")
 
     # FONT
-    pygame.font.init()
-    font = pygame.font.Font("Assets/NuosuSIL-Regular.ttf", 15)
+    init()
+    font = Font("Assets/NuosuSIL-Regular.ttf", 15)
 
     # IMAGES
-    gen_img = pygame.image.load('Assets/generate-button.png').convert_alpha()
+    gen_img = load('Assets/generate-button.png').convert_alpha()
 
     # BUTTON
     gen_button = Button(gen_img, screen)
 
     # NAME BOX
-    name_box_container = pygame.Rect(20, 20, screen_width - 40, (screen_height - 40) / 2)
+    name_box_container = Rect(20, 20, screen_width - 40, (screen_height - 40) / 2)
 
     # BOX COLOR
-    color = pygame.Color(1, 50, 32)
+    color = Color(1, 50, 32)
 
     # CREATE NAME DISPLAYS
     names = []
@@ -43,16 +48,16 @@ def main():
 
         screen.fill((255, 255, 255))
 
-        pygame.draw.rect(screen, color, name_box_container, 2)
+        draw.rect(screen, color, name_box_container, 2)
 
         gen_button.draw()
 
         for name in names:
             name.write_name()
 
-        pos = pygame.mouse.get_pos()
+        pos = mouse.get_pos()
 
-        for event in pygame.event.get():
+        for event in ev.get():
             if event.type == QUIT:
                 running = False
 
@@ -66,9 +71,9 @@ def main():
                     for name in names:
                         name.change_name(generate_name())
 
-        pygame.display.update()
+        update()
 
-    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":

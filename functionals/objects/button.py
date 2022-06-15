7ade5b2bb2d5e2158045abc3a113d4efdb08683a
Jonathon Meney <79815359+OsmiumCoder@users.noreply.github.com>
2022-06-14 22:01:07 -0300

    fully documented button class

--- functionals/objects/button.py
+++ functionals/objects/button.py
@@ -50,12 +50,19 @@ class Button:
         None
 
         """
+        # get width and height of screen to find position of button
         screen_width = self.screen.get_width()
         screen_height = self.screen.get_height()
 
+        # calculate X and Y position of the button
+        # such that it is centered in the X
+        # and 3/4 from the top of the screen
         button_position_x = screen_width / 2 - self.width / 2
         button_position_y = screen_height / 2 - self.height / 2 + screen_height / 4
 
+        # set the position of the Rect object of the image Surface
+        # so that the collision point will be at the buttons location
         self.rect.topleft = (button_position_x, button_position_y)
 
+        # draw the image at its Rect object collider position
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
